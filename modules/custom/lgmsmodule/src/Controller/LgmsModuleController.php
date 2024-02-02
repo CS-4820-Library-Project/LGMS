c454bf34679520ab021165b42e1738b1d94b0040
drupal <aymanerrarhiche@gmail.com>
2024-02-02 19:59:04 -0400

    all guides path gets the data from database and puts in a custom view

--- modules/custom/lgmsmodule/src/Controller/LgmsModuleController.php
+++ modules/custom/lgmsmodule/src/Controller/LgmsModuleController.php
@@ -5,6 +5,8 @@ namespace Drupal\lgmsmodule\Controller;
 use Drupal\Component\Render\FormattableMarkup;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Database\Connection;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Drupal\lgmsmodule\sql\sqlMethods;
@@ -62,4 +64,69 @@ class LgmsModuleController extends ControllerBase
 
     return $build;
   }
+
+  /**
+   * Returns a table of all Guides.
+   */
+  /**
+   * Returns a table of all Guides.
+   */
+  public function allGuides() {
+    $headers = [
+      $this->t('Guide'),
+      $this->t('Author'),
+      $this->t('Subject'),
+      $this->t('Type'),
+      $this->t('Last Updated'),
+    ];
+
+    // Load guides.
+    $nids = \Drupal::entityQuery('node')
+      ->condition('type', 'guide')
+      ->sort('title', 'ASC')
+      ->accessCheck(TRUE)
+      ->execute();
+
+    $nodes = \Drupal\node\Entity\Node::loadMultiple($nids);
+    $rows = [];
+    foreach ($nodes as $node) {
+      // Get the guide title with a link.
+      $url = Url::fromRoute('entity.node.canonical', ['node' => $node->id()]);
+      $link = Link::fromTextAndUrl($node->label(), $url)->toString();
+
+      // Get the author's name.
+      $author = $node->getOwner();
+      $author_name = $author ? $author->getDisplayName() : $this->t('Unknown');
+
+      // Get the subject and type.
+      $subject = $node->get('field_guide_subject')->entity;
+      $type = $node->get('field_guide_type')->entity;
+      $subject_name = $subject ? $subject->getName() : $this->t('N/A');
+      $type_name = $type ? $type->getName() : $this->t('N/A');
+
+      // Format the last updated time.
+      $last_updated = \Drupal::service('date.formatter')->format($node->getChangedTime(), 'custom', 'm/d/Y');
+
+      $rows[] = [
+        'data' => [
+          $link,
+          $author_name,
+          $subject_name,
+          $type_name,
+          $last_updated,
+        ],
+      ];
+    }
+
+    $build['table'] = [
+      '#type' => 'table',
+      '#header' => $headers,
+      '#rows' => $rows,
+      '#empty' => $this->t('No guides found'),
+    ];
+
+    return $build;
+  }
+
+
 }
