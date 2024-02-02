557a1ab234332db974a67653580dfdb14654f592
drupal <aymanerrarhiche@gmail.com>
2024-02-02 12:49:22 -0400

    get guides instead of articles

--- modules/custom/lgmsmodule/src/sql/sqlMethods.php
+++ modules/custom/lgmsmodule/src/sql/sqlMethods.php
@@ -12,24 +12,24 @@ class sqlMethods {
     $this->database = $database;
   }
 
-  public static function create(ContainerInterface $container)
-  {
+  public static function create(ContainerInterface $container) {
     return new static(
       $container->get('database')
     );
   }
 
-  public function getTitle(String $nid){
-    // Get the article title from the node_field_data table.
+  public function getTitle(String $nid) {
+    // Get the guide title from the node_field_data table.
     $query = $this->database->select('node_field_data', 'nd')
       ->fields('nd', ['title'])
-      ->condition('nd.nid', $nid);
+      ->condition('nd.nid', $nid)
+      ->condition('nd.type', 'guide'); // Added condition for 'guide' content type
 
     return $query->execute()->fetchField();
   }
 
-  public function getTaxonomyTerm(String $tid){
-    // Get the article title from the node_field_data table.
+  public function getTaxonomyTerm(String $tid) {
+    // Get the taxonomy term name from the taxonomy_term_field_data table.
     $query = $this->database->select('taxonomy_term_field_data', 'td')
       ->fields('td', ['name'])
       ->condition('td.tid', $tid);
@@ -37,31 +37,29 @@ class sqlMethods {
     return $query->execute()->fetchField();
   }
 
-  public function getFromTable(String $tableName): array
-  {
+  public function getFromTable(String $tableName): array {
     $splitName = explode('__', $tableName);
     $field_name = end($splitName);
     $termColName = $field_name . '_target_id';
 
+    // Assuming that the field is also attached to the 'guide' content type
     $query = $this->database->select($tableName, 'n')
-      ->condition('n.bundle', 'article')
+      ->condition('n.bundle', 'guide') // Changed from 'article' to 'guide'
       ->fields('n', ['entity_id', $termColName]);
 
     return $query->execute()->fetchAll();
   }
 
-  public function getGuides(): array
-  {
+  public function getGuides(): array {
     $query = $this->database->select('node_field_data', 'n')
-      ->condition('n.type', 'article')
+      ->condition('n.type', 'guide') // Changed from 'article' to 'guide'
       ->condition('n.status', 1)
       ->fields('n', ['title', 'uid', 'nid', 'changed']);
 
     return $query->execute()->fetchAllAssoc('nid');
   }
 
-  public function getOwner($uid)
-  {
+  public function getOwner($uid) {
     $query = $this->database->select('users_field_data', 'u')
       ->fields('u', ['name'])
       ->condition('u.uid', $uid);
