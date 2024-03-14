903fd1477b384d85e76cc0ac8f932dfd9a18fb94
drupal <aymanerrarhiche@gmail.com>
2024-03-14 11:38:38 -0300

    refactored the build of owner block

--- modules/custom/lgmsmodule/src/Plugin/Block/LgmsGuideOwnerBlock.php
+++ modules/custom/lgmsmodule/src/Plugin/Block/LgmsGuideOwnerBlock.php
@@ -31,133 +31,127 @@ class LgmsGuideOwnerBlock extends BlockBase {
     $node = \Drupal::routeMatch()->getParameter('node');
     $current_user = \Drupal::currentUser();
 
-    if (true) {
+    // Fetch the author's information.
+    $author = $node->getOwner();
+    $first_name = '';
+    $last_name = '';
+    $phone_number_clickable = '';
+    $email = $author->getEmail();
 
-      // Fetch the author's information.
-      $author = $node->getOwner();
-      $name = $author->getDisplayName();
-      $email = $author->getEmail();
-
-      // Prepare the markup for name, email and phone number.
-      $first_name = '';
-      $last_name = '';
-      $phone_number_clickable = '';
-
-      if($author->hasField('field_first_name')){
-        $first_name = $author->get('field_first_name')->value;
-        $last_name = $author->get('field_last_name')->value;
-        $phone_number_raw = $author->get('field_phone_number')->value;
-        $phone_number_formatted = $this->formatPhoneNumber($phone_number_raw);
-        $phone_number_clickable = $this->makePhoneNumberClickable($phone_number_raw, $phone_number_formatted);
-      }
-
-      // Fetch and display the guide's title.
-      $build['#title'] = $first_name . "'s Contact Information";
-
-      // Attempt to load the profile picture.
-      $user_picture = '';
-      if ($author->user_picture && !$author->user_picture->isEmpty()) {
-        $user_picture = $author->user_picture->entity->getFileUri();
-      }
-
-      // Initialize the author_info container.
-      $build['author_info'] = [
-        '#type' => 'container',
-        '#attributes' => ['class' => ['author-info']],
-      ];
-
-      // Add the profile picture to the author_info container if available and configured to show.
-      if ($this->configuration['show_user_picture'] && $author->user_picture && !$author->user_picture->isEmpty()) {
-        $user_picture = $author->user_picture->entity->getFileUri();
-        $build['author_info']['picture'] = [
-          '#theme' => 'image',
-          '#uri' => $user_picture,
-          '#attributes' => ['alt' => $this->t("@name's profile picture", ['@name' => $author->getDisplayName()])],
-          '#style' => ['width' => '100px'], // Adjust size as needed.
-        ];
-      }
-
-      $author_details_markup = "<p><strong>Name:</strong> {$first_name} {$last_name}</p>";
-
-      if ($this->configuration['show_email']) {
-        $author_details_markup .= "<p><strong>Email:</strong> <a href='mailto:{$email}'>{$email}</a></p>";
-      }
-
-      if ($this->configuration['show_phone']) {
-        $author_details_markup .= "<p><strong>Phone:</strong> {$phone_number_clickable}</p>";
-      }
-
-      // Fetch and prepare subjects.
-      $subjects = [];
-      if ($node instanceof Node) {
-        if ($node->bundle() === 'guide') {
-          // Directly fetch subjects from the guide.
-          if (!$node->get('field_lgms_guide_subject')->isEmpty()) {
-            foreach ($node->get('field_lgms_guide_subject')->referencedEntities() as $term) {
-              $subjects[] = $term->getName();
-            }
-          }
-        } elseif ($node->bundle() === 'guide_page') {
-          // Use the parent guide to find the subjects for a guide page.
-          $parent_guide = $node->get('field_parent_guide')->entity;
-          if ($parent_guide && !$parent_guide->get('field_lgms_guide_subject')->isEmpty()) {
-            foreach ($parent_guide->get('field_lgms_guide_subject')->referencedEntities() as $term) {
-              $subjects[] = $term->getName();
-            }
-          }
-        }
-      }
-
-      // Add subjects to the markup if available.
-      $subjectsMarkup = '';
-      if ($this->configuration['show_subjects'] && !empty($subjects)) {
-        $subjectsMarkup .= "<p><strong>Subjects:</strong></p><ul>";
-        foreach ($subjects as $subject) {
-          $subjectsMarkup .= "<li>{$subject}</li>";
-        }
-        $subjectsMarkup .= "</ul>";
-      }
-
-      // Adjust where you include $subjectsMarkup in your final build array as needed.
-      $author_details_markup .= $subjectsMarkup;
-
-      // Include the combined markup and subjects in the build.
-      $build['author_info']['details'] = [
-        '#markup' => $author_details_markup,
-      ];
-
-      // Check if there's body content configured and add it to the block build array
-      if (!empty($this->configuration['body']['value'])) {
-        $build['body'] = [
-          '#type' => 'processed_text',
-          '#text' => $this->configuration['body']['value'],
-          '#format' => $this->configuration['body']['format'],
-        ];
-      }
-
-      if ($current_user->isAuthenticated()) {
-        // Get the current URL
-        $current_url = \Drupal\Core\Url::fromRoute('<current>');
-        $destination = $current_url->toString();
-        $modal_form_url = \Drupal\Core\Url::fromRoute('lgmsmodule.owner_block_form', [], ['query' => ['destination' => $destination]]);
-
-        $build['link_to_modal_form'] = [
-          '#type' => 'link',
-          '#title' => $this->t('Edit Profile Information'),
-          '#url' => $modal_form_url,
-          '#attributes' => [
-            'class' => ['use-ajax', 'button'],
-            'data-dialog-type' => 'modal',
-            'data-dialog-options' => \Drupal\Component\Serialization\Json::encode(['width' => 700]),
-          ],
-        ];
-      }
-
-      $build['#attached']['library'][] = 'core/drupal.dialog.ajax';
-
+    // Check for first name, last name, and phone number fields and format accordingly.
+    if($author->hasField('field_first_name')){
+      $first_name = $author->get('field_first_name')->value;
+      $last_name = $author->get('field_last_name')->value;
+      $phone_number_raw = $author->get('field_phone_number')->value;
+      $phone_number_formatted = $this->formatPhoneNumber($phone_number_raw);
+      $phone_number_clickable = $this->makePhoneNumberClickable($phone_number_raw, $phone_number_formatted);
     }
+
+    // Set the title using the first name.
+    $build['#title'] = $first_name . "'s Contact Information";
+
+    // Initialize the author_info container with profile picture if available.
+    $build['author_info'] = [
+      '#type' => 'container',
+      '#attributes' => ['class' => ['author-info']],
+    ];
+    if ($this->configuration['show_user_picture'] && $author->user_picture && !$author->user_picture->isEmpty()) {
+      $user_picture_uri = $author->user_picture->entity->getFileUri();
+      $build['author_info']['picture'] = [
+        '#theme' => 'image',
+        '#uri' => $user_picture_uri,
+        '#attributes' => ['alt' => $this->t("@name's profile picture", ['@name' => $author->getDisplayName()])],
+        '#style' => ['width' => '100px'],
+      ];
+    }
+
+    // Construct the author details markup.
+    $author_details_markup = "<p style='margin-left: 20px; !important'><strong>Name:</strong> {$first_name} {$last_name}</p>";
+    if ($this->configuration['show_email']) {
+      $author_details_markup .= "<p><strong>Email:</strong> <a href='mailto:{$email}'>{$email}</a></p>";
+    }
+    if ($this->configuration['show_phone']) {
+      $author_details_markup .= "<p><strong>Phone:</strong> {$phone_number_clickable}</p>";
+    }
+
+    // Add subjects to the author details if configured to show.
+    if ($this->configuration['show_subjects']) {
+      $subjectsMarkup = $this->fetchSubjectsMarkup($node);
+      $author_details_markup .= $subjectsMarkup;
+    }
+
+    $build['author_info']['details'] = ['#markup' => $author_details_markup];
+
+    // Include body content if configured.
+    if (!empty($this->configuration['body']['value'])) {
+      $build['body'] = [
+        '#type' => 'processed_text',
+        '#text' => $this->configuration['body']['value'],
+        '#format' => $this->configuration['body']['format'],
+      ];
+    }
+
+    // Provide a link to edit profile information for authenticated users.
+    if ($current_user->isAuthenticated()) {
+      $build += $this->addEditProfileLink();
+    }
+
+    $build['#attached']['library'][] = 'core/drupal.dialog.ajax';
+
     return $build;
   }
+
+  private function fetchSubjectsMarkup($node) {
+    $subjects = [];
+    // Fetch subjects based on node type and bundle.
+    if ($node->bundle() === 'guide') {
+      $subjects = $this->extractSubjects($node, 'field_lgms_guide_subject');
+    } elseif ($node->bundle() === 'guide_page') {
+      $parent_guide = $node->get('field_parent_guide')->entity;
+      if ($parent_guide) {
+        $subjects = $this->extractSubjects($parent_guide, 'field_lgms_guide_subject');
+      }
+    }
+
+    $subjectsMarkup = '';
+    if (!empty($subjects)) {
+      $subjectsMarkup .= "<p><strong>Subjects:</strong></p><ul>";
+      foreach ($subjects as $subject) {
+        $subjectsMarkup .= "<li>{$subject}</li>";
+      }
+      $subjectsMarkup .= "</ul>";
+    }
+    return $subjectsMarkup;
+  }
+
+  private function extractSubjects($entity, $field_name) {
+    $subjects = [];
+    if (!$entity->get($field_name)->isEmpty()) {
+      foreach ($entity->get($field_name)->referencedEntities() as $term) {
+        $subjects[] = $term->getName();
+      }
+    }
+    return $subjects;
+  }
+
+  private function addEditProfileLink() {
+    $current_url = \Drupal\Core\Url::fromRoute('<current>');
+    $destination = $current_url->toString();
+    $modal_form_url = \Drupal\Core\Url::fromRoute('lgmsmodule.owner_block_form', [], ['query' => ['destination' => $destination]]);
+    return [
+      'link_to_modal_form' => [
+        '#type' => 'link',
+        '#title' => $this->t('Edit Profile Information'),
+        '#url' => $modal_form_url,
+        '#attributes' => [
+          'class' => ['use-ajax', 'button'],
+          'data-dialog-type' => 'modal',
+          'data-dialog-options' => \Drupal\Component\Serialization\Json::encode(['width' => 700]),
+        ],
+      ]
+    ];
+  }
+
 
   /**
    * Makes a phone number clickable for devices that support dialing.
