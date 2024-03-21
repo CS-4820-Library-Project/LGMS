294bda0f3d9574a70dc4ec6bf8307c82b46679d4
drupal <aymanerrarhiche@gmail.com>
2024-03-21 15:27:58 -0300

    fixed the guide owner block, styling & field wise

--- modules/custom/lgmsmodule/src/Plugin/Block/LgmsGuideOwnerBlock.php
+++ modules/custom/lgmsmodule/src/Plugin/Block/LgmsGuideOwnerBlock.php
@@ -29,7 +29,13 @@ class LgmsGuideOwnerBlock extends BlockBase {
   {
     $build = [];
     $node = \Drupal::routeMatch()->getParameter('node');
+    if (!$node) {
+      return $build; // Early return if there's no node context.
+    }
+
     $current_user = \Drupal::currentUser();
+    // Initialize an array to hold our elements
+    $elements = [];
 
     // Fetch the author's information.
     $author = $node->getOwner();
@@ -55,7 +61,6 @@ class LgmsGuideOwnerBlock extends BlockBase {
       }
     }
 
-
     // Set the title using the first name.
     $build['#title'] = $first_name . "'s Contact Information";
 
@@ -64,50 +69,123 @@ class LgmsGuideOwnerBlock extends BlockBase {
       '#type' => 'container',
       '#attributes' => ['class' => ['author-info']],
     ];
-    if ($this->configuration['show_user_picture'] && $author->hasField('field_lgms_user_picture') && !$author->get('field_lgms_user_picture')->isEmpty()) {
-      $user_picture_file = $author->get('field_lgms_user_picture')->entity;
-      if ($user_picture_file) {
+
+    // Full name
+    $build['author_info']['author_name'] = [
+      '#type' => 'html_tag',
+      '#tag' => 'p',
+      '#value' => $this->t('<strong>Name:</strong> @first_name @last_name', [
+        '@first_name' => $first_name,
+        '@last_name' => $last_name
+      ]),
+      '#attributes' => [
+        'style' => 'margin-left: 16px; margin-top: 8px;',
+      ],
+    ];
+
+    // User picture
+    if ($author->hasField('user_picture') && !$author->get('user_picture')->isEmpty()) {
+      $user_picture_file = $author->get('user_picture')->entity;
+      if ($user_picture_file && $this->configuration['show_user_picture']) {
         $user_picture_uri = $user_picture_file->getFileUri();
-        $build['author_info']['picture'] = [
-          '#theme' => 'image',
-          '#uri' => $user_picture_uri,
-          '#attributes' => ['alt' => $this->t("@name's profile picture", ['@name' => $author->getDisplayName()])],
+        $elements['user_picture'] = [
+          'content' => [
+            '#theme' => 'image',
+            '#uri' => $user_picture_uri,
+            '#attributes' => [
+              'alt' => $this->t("@name's profile picture", ['@name' => $author->getDisplayName()]),
+              'loading' => 'lazy',
+              'style' => 'margin-left: 16px; margin-right: 16px; margin-bottom: 16px; border: 4px solid #fefefe; border-radius: 6px; box-shadow: 0 0 0 1px rgba(51,51,51,.2);',
+            ],
+          ],
+          'position' => $this->configuration['position_user_picture'],
         ];
+
       }
     }
 
-    // Construct the author details markup.
-    $author_details_markup = "<p><strong>Name:</strong> {$first_name} {$last_name}</p>";
+    // Email
     if ($this->configuration['show_email']) {
-      $author_details_markup .= "<p><strong>Email:</strong> <a href='mailto:{$email}'>{$email}</a></p>";
-    }
-    if ($this->configuration['show_phone']) {
-      $author_details_markup .= "<p><strong>Phone:</strong> {$phone_number_clickable}</p>";
-    }
-
-    // Add subjects to the author details if configured to show.
-    if ($this->configuration['show_subjects']) {
-      $subjectsMarkup = $this->fetchSubjectsMarkup($node);
-      $author_details_markup .= $subjectsMarkup;
-    }
-
-    $build['author_info']['details'] = ['#markup' => $author_details_markup];
-
-    // Include body content if configured.
-    if (!empty($this->configuration['body']['value'])) {
-      $build['body'] = [
-        '#type' => 'processed_text',
-        '#text' => $this->configuration['body']['value'],
-        '#format' => $this->configuration['body']['format'],
+      $email = $author->getEmail();
+      $elements['email'] = [
+        'content' => [
+          '#type' => 'html_tag',
+          '#tag' => 'p',
+          '#value' => $this->t('<strong>Email:</strong> <a href="mailto:@email">@email</a>', ['@email' => $email]),
+          '#attributes' => ['style' => 'margin-left: 16px;'],
+        ],
+        'position' => $this->configuration['position_email'],
       ];
     }
 
-    // Provide a link to edit profile information for authenticated users.
+    // Phone Number
+    if ($author->hasField('field_lgms_phone_number') && !$author->get('field_lgms_phone_number')->isEmpty() && $this->configuration['show_phone']) {
+      $phone_number_raw = $author->get('field_lgms_phone_number')->value;
+      $phone_number_clickable = $this->makePhoneNumberClickable($phone_number_raw, $this->formatPhoneNumber($phone_number_raw));
+      $elements['phone_number'] = [
+        'content' => [
+          '#type' => 'html_tag',
+          '#tag' => 'p',
+          '#value' => "<strong>Phone:</strong> {$phone_number_clickable}",
+          '#attributes' => [
+            'style' => 'margin-left: 16px;',
+          ],
+        ],
+        'position' => $this->configuration['position_phone_number'],
+      ];
+    }
+
+    // Subjects
+    if ($this->configuration['show_subjects']) {
+      $subjectsMarkup = $this->fetchSubjectsMarkup($node);
+      $elements['subjects'] = [
+        'content' => [
+          '#type' => 'container',
+          '#attributes' => ['style' => 'margin-left: 16px;'],
+          'children' => ['#markup' => $subjectsMarkup],
+        ],
+        'position' => $this->configuration['position_subjects'],
+      ];
+    }
+
+    // Body
+    if ($author->hasField('field_lgms_body') && !$author->get('field_lgms_body')->isEmpty() && $this->configuration['show_body']) {
+      $body_value = $author->get('field_lgms_body')->value;
+      $body_format = $author->get('field_lgms_body')->format;
+      $elements['body'] = [
+        'content' => [
+          '#type' => 'container',
+          '#attributes' => [
+            'style' => 'margin-left: 16px;',
+          ],
+          'children' => [
+            '#type' => 'processed_text',
+            '#text' => $body_value,
+            '#format' => $body_format,
+          ],
+        ],
+        'position' => $this->configuration['position_body'],
+      ];
+    }
+
+
+    // Sort the elements by their position
+    uasort($elements, function ($a, $b) {
+      return $a['position'] <=> $b['position'];
+    });
+
+    // Add sorted elements to the build array
+    foreach ($elements as $key => $element) {
+      $build[$key] = $element['content'];
+    }
+
+    // Add the edit profile link for authenticated users
     if ($current_user->isAuthenticated()) {
-      $build += $this->addEditProfileLink();
+      $build['edit_profile_link'] = $this->addEditProfileLink();
     }
 
     $build['#attached']['library'][] = 'core/drupal.dialog.ajax';
+    $build['#attached']['library'][] = 'lgmsmodule/lgmsmodule';
 
     return $build;
   }
@@ -151,25 +229,27 @@ class LgmsGuideOwnerBlock extends BlockBase {
     return $subjects;
   }
 
-  private function addEditProfileLink(): array
-  {
-    $current_url = \Drupal\Core\Url::fromRoute('<current>');
-    $destination = $current_url->toString();
-    $modal_form_url = \Drupal\Core\Url::fromRoute('lgmsmodule.owner_block_form', [], ['query' => ['destination' => $destination]]);
+  private function addEditProfileLink(): array {
+    // Get the current user's ID.
+    $user_id = \Drupal::currentUser()->id();
+
+    // Get the current request URI to use as the destination after saving the profile.
+    $destination = \Drupal::request()->getRequestUri();
+
+    // Construct the URL to the user's edit page, including the destination parameter.
+    $user_edit_url = \Drupal\Core\Url::fromRoute('entity.user.edit_form', ['user' => $user_id], ['query' => ['destination' => $destination]]);
+
     return [
-      'link_to_modal_form' => [
+      'link_to_user_edit' => [
         '#type' => 'link',
-        '#title' => $this->t('Edit Profile Information'),
-        '#url' => $modal_form_url,
+        '#title' => $this->t('Edit Your Profile'),
+        '#url' => $user_edit_url,
         '#attributes' => [
-          'class' => ['use-ajax', 'button'],
-          'data-dialog-type' => 'modal',
-          'data-dialog-options' => \Drupal\Component\Serialization\Json::encode(['width' => 700]),
+          'class' => ['button'],
         ],
       ]
     ];
   }
-
 
   /**
    * Makes a phone number clickable for devices that support dialing.
@@ -220,38 +300,79 @@ class LgmsGuideOwnerBlock extends BlockBase {
   {
     $form = parent::blockForm($form, $form_state);
 
+    // Define the number of positions based on the number of elements you have.
+    $positions = range(1, 5); // Adjust the number 5 based on your actual number of elements.
+    $position_options = array_combine($positions, $positions);
+
     $form['show_user_picture'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Show User Picture'),
       '#default_value' => $this->configuration['show_user_picture'],
+      '#description' => $this->t('Check this box to display the user’s profile picture.'),
     ];
 
-    // Add a checkbox to toggle the visibility of the email address
     $form['show_email'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Show Email'),
       '#default_value' => $this->configuration['show_email'] ?? TRUE,
+      '#description' => $this->t('Check this box to display the user’s email address.'),
     ];
 
-    // Add a checkbox to control the visibility of the phone number.
     $form['show_phone'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Show Phone Number'),
       '#default_value' => $this->configuration['show_phone'],
+      '#description' => $this->t('Check this box to display the user’s phone number.'),
     ];
 
     $form['show_subjects'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Show Subjects'),
       '#default_value' => $this->configuration['show_subjects'],
+      '#description' => $this->t('Check this box to display the subjects associated with the content.'),
     ];
 
-    $form['body'] = [
-      '#type' => 'text_format',
-      '#title' => $this->t('Body'),
-      '#default_value' => $this->configuration['body']['value'] ?? '',
-      '#format' => $this->configuration['body']['format'] ?? 'basic_html',
-      '#description' => $this->t('Add optional body content. This will be displayed in the block.'),
+    $form['show_body'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Show Body'),
+      '#default_value' => $this->configuration['show_body'],
+      '#description' => $this->t('Check this box to display the body content from the user’s profile.'),
+    ];
+
+    // Position selectors
+    $form['position_user_picture'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Position for User Picture'),
+      '#options' => $position_options,
+      '#default_value' => $this->configuration['position_user_picture'] ?? 1,
+    ];
+
+    $form['position_email'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Position for Email'),
+      '#options' => $position_options,
+      '#default_value' => $this->configuration['position_email'] ?? 2,
+    ];
+
+    $form['position_phone_number'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Position for Phone Number'),
+      '#options' => $position_options,
+      '#default_value' => $this->configuration['position_phone_number'] ?? 3,
+    ];
+
+    $form['position_subjects'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Position for Subjects'),
+      '#options' => $position_options,
+      '#default_value' => $this->configuration['position_subjects'] ?? 4,
+    ];
+
+    $form['position_body'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Position for Body'),
+      '#options' => $position_options,
+      '#default_value' => $this->configuration['position_body'] ?? 5,
     ];
 
     return $form;
@@ -264,7 +385,14 @@ class LgmsGuideOwnerBlock extends BlockBase {
     $this->configuration['show_email'] = $form_state->getValue('show_email');
     $this->configuration['show_phone'] = $form_state->getValue('show_phone');
     $this->configuration['show_subjects'] = $form_state->getValue('show_subjects');
-    $this->configuration['body'] = $form_state->getValue('body');
+    $this->configuration['show_body'] = (bool) $form_state->getValue('show_body');
+
+    // Save position settings
+    $this->configuration['position_user_picture'] = $form_state->getValue('position_user_picture');
+    $this->configuration['position_email'] = $form_state->getValue('position_email');
+    $this->configuration['position_phone_number'] = $form_state->getValue('position_phone_number');
+    $this->configuration['position_subjects'] = $form_state->getValue('position_subjects');
+    $this->configuration['position_body'] = $form_state->getValue('position_body');
   }
 
   public function defaultConfiguration(): array
@@ -274,10 +402,12 @@ class LgmsGuideOwnerBlock extends BlockBase {
         'show_phone' => TRUE,
         'show_subjects' => TRUE,
         'show_user_picture' => TRUE,
-        'body' => [
-          'value' => '',
-          'format' => 'basic_html',
-        ],
+        'show_body' => TRUE,
+        'position_user_picture' => 1,
+        'position_email' => 2,
+        'position_phone_number' => 3,
+        'position_subjects' => 4,
+        'position_body' => 5,
       ];
   }
 }
