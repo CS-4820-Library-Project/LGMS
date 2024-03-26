9f1ece763843c5000a7b01d58f5726b07a1e17c2
Alwalid-Abushanab <aabushanab@upei.ca>
2024-03-26 20:47:03 -0300

    refactor content item deletion to use 1 form

--- modules/custom/lgmsmodule/src/Form/DeleteHTMLForm.php
+++ modules/custom/lgmsmodule/src/Form/DeleteHTMLForm.php
@@ -38,6 +38,29 @@ class DeleteHTMLForm extends FormBase {
       '#type' => 'hidden',
       '#value' => $current_item,
     ];
+    $current_item = Node::load($current_item);
+
+    $possible_fields = [
+      'field_database_item',
+      'field_html_item',
+      'field_book_item',
+      'field_media_image'
+    ];
+
+    $field_to_delete = '';
+
+    foreach ($possible_fields as $field_name) {
+      if (!$current_item->get($field_name)->isEmpty()) {
+        $field_to_delete = $field_name;
+        break;
+      }
+    }
+
+    $form['field_name'] = [
+      '#type' => 'hidden',
+      '#value' => $field_to_delete,
+    ];
+
 
     $title = $this->t('<Strong>Are you Sure you want to Delete This Item?</Strong>');
 
@@ -69,13 +92,57 @@ class DeleteHTMLForm extends FormBase {
   public function submitAjax(array &$form, FormStateInterface $form_state) {
     $ajaxHelper = new FormHelper();
 
-    return $ajaxHelper->submitModalAjax($form, $form_state, 'Box was deleted Successfully.');
+    return $ajaxHelper->submitModalAjax($form, $form_state, 'Item was deleted Successfully.');
   }
 
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $ajaxHelper = new FormHelper();
-    $ajaxHelper->delete_item($form, $form_state, 'field_html_item');
+    $current_box = Node::load($form_state->getValue('current_box'));
+    $current_item = Node::load($form_state->getValue('current_item'));
+    $field_name = $form_state->getValue('field_name');
 
+    // remove link to current box
+    $child_items = $current_box->get('field_box_items')->getValue();
+    $child_items = array_filter($child_items, function ($item) use ($current_item) {
+      return $item['target_id'] != $current_item->id();
+    });
+
+    $current_box->set('field_box_items', $child_items);
+    $current_box->save();
+
+    $field = $current_item->get($field_name)->entity;
+
+    // If this is the box the original item was created in
+    if($field->hasField('field_parent_item') && $current_item->id() == $field->get('field_parent_item')->entity->id()){
+      // Get all guide_item that point to this field
+      $query = \Drupal::entityQuery('node')
+        ->condition('type', 'guide_item')
+        ->condition($field_name, $field->id())
+        ->accessCheck(false);
+      $result = $query->execute();
+
+      // Go though all items that reference the given node and delete them
+      foreach ($result as $item){
+        $item = Node::load($item);
+        $parent_box = $item->get('field_parent_box')->entity;
+
+        // remove link to current box
+        $child_items = $parent_box->get('field_box_items')->getValue();
+        $child_items = array_filter($child_items, function ($box) use ($item) {
+          return $box['target_id'] != $item->id();
+        });
+
+        $parent_box->set('field_box_items', $child_items);
+        $parent_box->save();
+
+        $item?->delete();
+      }
+
+      $field?->delete();
+    }
+
+    $current_item?->delete();
+
+    $ajaxHelper = new FormHelper();
     $ajaxHelper->updateParent($form, $form_state);
   }
 }
