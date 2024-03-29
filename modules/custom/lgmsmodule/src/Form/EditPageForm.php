5dd2c1f03b6f30886508ea835921cc2934b2b207
Alwalid-Abushanab <aabushanab@upei.ca>
2024-03-29 18:15:38 -0300

    refactor EditPageForm.php and fix position field is required error.

--- modules/custom/lgmsmodule/src/Form/EditPageForm.php
+++ modules/custom/lgmsmodule/src/Form/EditPageForm.php
@@ -18,32 +18,20 @@ class EditPageForm extends FormBase {
 
   public function buildForm(array $form, FormStateInterface $form_state)
   {
-    $form['#prefix'] = '<div id="' . $this->getFormId() . '">';
-    $form['#suffix'] = '</div>';
-    $form['messages'] = [
-      '#weight' => -9999,
-      '#type' => 'status_messages',
-    ];
+    $form_helper = new FormHelper();
+    $form_helper->set_prefix($form,  $this->getFormId());
 
     $current_guide = \Drupal::request()->query->get('guide_id');
     if ($current_guide) {
       $current_guide = Node::load($current_guide);
     }
 
-
-
     if ($current_guide) {
-      $form['current_node'] = [
-        '#type' => 'hidden',
-        '#value' => $current_guide->id(),
-      ];
-
       $form['select_page'] = [
         '#type' => 'select',
         '#title' => $this->t('Select Page'),
-        '#options' => $this->getPageList($current_guide->id(), true),
+        '#options' => $this->getPageList($form_state, $current_guide->id(), true),
         '#empty_option' => $this->t('- Select a Page -'),
-        '#validated' => TRUE,
         '#required' => TRUE,
         '#ajax' => [
           'callback' => '::selectPageCallBack',
@@ -57,81 +45,91 @@ class EditPageForm extends FormBase {
         '#attributes' => ['id' => 'update-wrapper'],
       ];
 
-      // Title field pre-filled with the existing title.
-      $form['update_wrapper']['title'] = [
-        '#type' => 'textfield',
-        '#title' => $this->t('Page Title'),
-        '#required' => TRUE,
-      ];
-
-      // Description field pre-filled with the existing body.
-      $form['update_wrapper']['hide_description'] = [
-        '#type' => 'checkbox',
-        '#title' => $this->t('Hide description'),
-        '#ajax' => [
-          'callback' => '::hideDescriptionCallback',
-          'wrapper' => 'update-wrapper', // This should be the ID of the element you want to replace or update, you can adjust as needed.
-          'event' => 'change',
-        ],
-      ];
-
-      // Description field with state controlled by hide_description checkbox.
-      $form['update_wrapper']['description'] = [
-        '#type' => 'text_format',
-        '#title' => $this->t('Description'),
-        '#states' => [
-          'invisible' => [
-            ':input[name="hide_description"]' => ['checked' => TRUE],
-          ],
-        ],
-      ];
-
-      $form['update_wrapper']['position'] = [
-        '#type' => 'select',
-        '#title' => $this->t('Position'),
-        '#options' => $this->getPageList($current_guide->id(), false),
-        '#required' => TRUE,
-      ];
-
-      /**Follow This**/
       $selected = !empty($form_state->getValue('select_page')) ? $form_state->getValue('select_page') : '';
 
-      if($selected != ''){
-        $selected = Node::load($selected);
-        if((!$selected->hasField('field_reference_node') || $selected->get('field_reference_node')->isEmpty())){
-          $form['update_wrapper']['description']['#value'] = $selected->get('field_description')->value;
-        } else {
-          $form['update_wrapper']['description']['#attributes']['disabled'] = true;
-        }
-        if($selected->hasField('field_parent_guide')) {
-          //$form['update_wrapper']['position']['#value'] = $selected->get('field_parent_guide')->entity->id();
-        }
+
+      if($selected != '') {
+        $selected_node = Node::load($selected);
+        $reference = !$selected_node->get('field_reference_node')->isEmpty();
+
+        $form['update_wrapper']['current_node'] = [
+          '#type' => 'hidden',
+          '#value' => $selected_node->id(),
+        ];
+
+        // Title field pre-filled with the existing title.
+        $form['update_wrapper']['title'] = [
+          '#type' => 'textfield',
+          '#title' => $this->t('Page Title'),
+          '#value' => $reference? t('This is Just a Reference and can not be Edited') : $selected_node->label(),
+          '#required' => !$reference,
+          '#disabled' => $reference,
+        ];
+
+        // Description field pre-filled with the existing body.
+        $form['update_wrapper']['hide_description'] = [
+          '#type' => 'checkbox',
+          '#title' => $this->t('Hide description'),
+          '#default_value' => $selected_node->get('field_hide_description')->value,
+          '#disabled' => $reference,
+          '#ajax' => [
+            'callback' => '::hideDescriptionCallback',
+            'wrapper' => 'update-wrapper', // This should be the ID of the element you want to replace or update, you can adjust as needed.
+            'event' => 'change',
+          ],
+        ];
+
+        // Description field with state controlled by hide_description checkbox.
+        $form['update_wrapper']['description'] = [
+          '#type' => 'text_format',
+          '#title' => $this->t('Description'),
+          '#states' => [
+            'invisible' => [
+              ':input[name="hide_description"]' => ['checked' => TRUE],
+            ],
+            'required' => [
+              ':input[name="hide_description"]' => ['checked' => False],
+            ],
+          ],
+          '#default_value' => $selected_node->get('field_description')->value,
+          '#disabled' => $reference,
+        ];
+
+        $form['update_wrapper']['position'] = [
+          '#type' => 'select',
+          '#title' => $this->t('Position'),
+          '#options' => $this->getPageList($form_state, $current_guide->id(), false),
+          '#value' => !$selected_node->get('field_child_pages')->isEmpty()? $current_guide->id() : $selected_node->get('field_parent_guide')->entity->id(),
+          '#disabled' => !$selected_node->get('field_child_pages')->isEmpty(),
+          '#required' => TRUE,
+        ];
+
+        $disable = !$selected_node->get('field_reference_node')->isEmpty() && !$selected_node->get('field_reference_node')->entity->isPublished();
+
+        $form['update_wrapper']['draft_mode'] = [
+          '#type' => 'checkbox',
+          '#title' => $this->t('Draft Mode'),
+          '#description' => $disable? $this->t('The referenced page is unPublished. Publish it to be able to update this Page.') : $this->t('Check this box if the page is still in draft mode.'),
+          '#default_value' => !$selected_node->isPublished(),
+          '#disabled' => $disable,
+        ];
       }
-
-      $form['update_wrapper']['draft_mode'] = [
-        '#type' => 'checkbox',
-        '#title' => $this->t('Draft Mode'),
-        '#description' => $this->t('Check this box if the page is still in draft mode.'),
-      ];
-
-
-      $form['actions']['submit'] = [
-        '#type' => 'submit',
-        '#value' => $this->t('Edit'),
-        '#button_type' => 'primary',
-      ];
-
-      $form['actions']['submit']['#ajax'] = [
-        'callback' => '::submitAjax',
-        'event' => 'click',
-      ];
-
     } else {
       $form['error'] = [
         '#type' => 'markup',
         '#markup' => $this->t('The page could not be found.'),
       ];
     }
+
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Edit'),
+      '#button_type' => 'primary',
+      '#ajax' => [
+        'callback' => '::submitAjax',
+        'event' => 'click',
+      ],
+    ];
 
     return $form;
   }
@@ -154,40 +152,6 @@ class EditPageForm extends FormBase {
 
 
   public function selectPageCallBack(array &$form, FormStateInterface $form_state) {
-    $selected_page = $form_state->getValue('select_page');
-
-    // Check if a page is selected and it's not the empty option.
-    if (!empty($selected_page)) {
-      // Load the selected page node to check its field_child_pages.
-      $page_node = Node::load($selected_page);
-      if ($page_node && (!$page_node->hasField('field_reference_node') || $page_node->get('field_reference_node')->isEmpty())) {
-        $form['update_wrapper']['title']['#value'] = $page_node->label();
-        $form['update_wrapper']['hide_description']['#checked'] = $page_node->get('field_hide_description')->value;
-        $form['update_wrapper']['draft_mode']['#checked'] = !$page_node->isPublished();
-
-
-        $child_pages = $page_node->get('field_child_pages')->referencedEntities();
-
-        if (!empty($child_pages)) {
-          $guide = $form_state->getValue('current_node');
-          $guide = Node::load($guide);
-          $form['update_wrapper']['position']['#options'] = ['Guide' => [$guide->id() => $guide->label()]];
-        } else {
-          if($page_node->hasField('field_parent_guide')) {
-            $form['update_wrapper']['position']['#value'] = $page_node->get('field_parent_guide')->entity->id();
-          }
-        }
-      } else {
-        $form['update_wrapper']['title']['#value'] = 'This is Just a Reference and can not be Edited';
-        $form['update_wrapper']['title']['#attributes']['disabled'] = true;
-        $form['update_wrapper']['hide_description']['#attributes']['disabled'] = true;
-        $form['update_wrapper']['draft_mode']['#attributes']['disabled'] = true;
-        $form['update_wrapper']['position']['#attributes']['disabled'] = true;
-      }
-    } else {
-
-    }
-
     return $form['update_wrapper'];
   }
 
@@ -195,45 +159,49 @@ class EditPageForm extends FormBase {
     $selected_page = $form_state->getValue('select_page');
     $selected_page = Node::load($selected_page);
 
-    $selected_page->setTitle($form_state->getValue('title'));
-    $selected_page->set('field_hide_description', $form_state->getValue('select_page'));
-    $selected_page->set('field_description', [
-      'value' => $form_state->getValue('description')['value'],
-      'format' => $form_state->getValue('description')['format'],
-    ]);
-    $form_state->getValue('draft_mode') == '0'? $selected_page->setPublished(): $selected_page->setUnpublished();
-
-
-    if($selected_page->hasField('field_parent_guide')){
-      $parent = $selected_page->get('field_parent_guide')->entity;
-
-      $child_pages = $parent->get('field_child_pages')->getValue();
-
-      $child_pages = array_filter($child_pages, function ($page) use ($selected_page) {
-        return $page['target_id'] != $selected_page->id();
-      });
-
-      $parent->set('field_child_pages', $child_pages);
-      $parent->save();
-
-      $new_parent = Node::load($form_state->getValue('position'));
-      $selected_page->set('field_parent_guide', $new_parent);
+    if (!$selected_page->get('field_reference_node')->isEmpty()){
+      $form_state->getValue('draft_mode') == '0'? $selected_page->setPublished(): $selected_page->setUnpublished();
       $selected_page->save();
+    } else {
+      $selected_page->setTitle($form_state->getValue('title'));
+      $selected_page->set('field_hide_description', $form_state->getValue('hide_description') == '1');
+      $selected_page->set('field_description', [
+        'value' => $form_state->getValue('description')['value'],
+        'format' => $form_state->getValue('description')['format'],
+      ]);
+      $form_state->getValue('draft_mode') == '0'? $selected_page->setPublished(): $selected_page->setUnpublished();
 
-      $page_list = $new_parent->get('field_child_pages')->getValue();
-      $page_list[] = ['target_id' => $selected_page->id()];
 
-      $new_parent->set('field_child_pages', $page_list);
-      $new_parent->set('changed', \Drupal::time()->getRequestTime());
-      $new_parent->save();
+      if($selected_page->hasField('field_parent_guide')){
+        $parent = $selected_page->get('field_parent_guide')->entity;
+
+        $child_pages = $parent->get('field_child_pages')->getValue();
+
+        $child_pages = array_filter($child_pages, function ($page) use ($selected_page) {
+          return $page['target_id'] != $selected_page->id();
+        });
+
+        $parent->set('field_child_pages', $child_pages);
+        $parent->save();
+
+        $new_parent = Node::load($form_state->getValue('position'));
+        $selected_page->set('field_parent_guide', $new_parent);
+        $selected_page->save();
+
+        $page_list = $new_parent->get('field_child_pages')->getValue();
+        $page_list[] = ['target_id' => $selected_page->id()];
+
+        $new_parent->set('field_child_pages', $page_list);
+        $new_parent->set('changed', \Drupal::time()->getRequestTime());
+        $new_parent->save();
+      }
     }
-
 
     $ajaxHelper = new FormHelper();
     $ajaxHelper->updateParent($form, $form_state);
   }
 
-  public function getPageList($guide_id, $include_sub) {
+  public function getPageList(FormStateInterface $form_state, $guide_id, $include_sub) {
     $options = [];
     $guide = Node::load($guide_id);
 
@@ -245,6 +213,10 @@ class EditPageForm extends FormBase {
 
       if (!empty($child_pages)) {
         // Group label for child pages.
+        if (!$include_sub){
+          $options['Page Level'][$guide_id] = t('Page Level');
+        }
+
         $group_label = $include_sub? 'Pages' : 'Subpage Of';
 
         // Initialize the group if it's not set.
@@ -254,6 +226,10 @@ class EditPageForm extends FormBase {
 
         // Create options array from the child pages.
         foreach ($child_pages as $child_page) {
+          if (!$include_sub && $child_page->id() == $form_state->getValue('select_page')){
+            continue;
+          }
+
           if ($child_page->get('field_parent_guide')->entity->id() == $guide_id) {
             $options[$group_label][$child_page->id()] = $child_page->label();
 
